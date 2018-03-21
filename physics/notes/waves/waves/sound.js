0131caf19e92e6ebd4552c68d79eddc1dd0a737c
landgreen <rosslandgreen@gmail.com>
2018-03-21 06:17:48 -0700

    added color, and spelling

---
+++ physics/notes/waves/waves/sound.js
@@ -0,0 +1,245 @@
+(function() {
+  var canvas = document.getElementById("sound1");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "300 24px Roboto";
+  ctx.fillStyle = "#aaa";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("click to start simulation", canvas.width / 2, canvas.height / 2);
+})();
+(function() {
+  var canvas = document.getElementById("sound2");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "300 24px Roboto";
+  ctx.fillStyle = "#aaa";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("click to start simulation", canvas.width / 2, canvas.height / 2);
+})();
+(function() {
+  var canvas = document.getElementById("sound3");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "300 24px Roboto";
+  ctx.fillStyle = "#aaa";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("click to start simulation", canvas.width / 2, canvas.height / 2);
+})();
+
+function sound(el, speedOpacity = true, waveOut = false, drawOne = false) {
+  el.onclick = null; //stops the function from running on button click
+  const canvas = el;
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth - 20;
+
+  let pause = false;
+  el.addEventListener("mouseleave", function() {
+    pause = true;
+  });
+  el.addEventListener("mouseenter", function() {
+    pause = false;
+    if (!pause) requestAnimationFrame(cycle);
+  });
+  el.addEventListener("click", function() {
+    if (!settings.pusherReady) {
+      settings.pusherReady = true;
+    }
+  });
+
+  const Engine = Matter.Engine,
+    World = Matter.World,
+    Events = Matter.Events,
+    Composites = Matter.Composites,
+    Composite = Matter.Composite,
+    Constraint = Matter.Constraint,
+    Vertices = Matter.Vertices,
+    Query = Matter.Query,
+    Body = Matter.Body,
+    Bodies = Matter.Bodies;
+
+  // create an engine
+  const engine = Engine.create();
+  engine.world.gravity.scale = 0; //turn off gravity
+  engine.constraintIterations = 1;
+  engine.positionIterations = 1;
+  engine.velocityIterations = 1;
+
+  const settings = {
+    width: canvas.width,
+    height: canvas.height,
+    radius: 5,
+    rows: Math.floor(canvas.width / 10),
+    columns: 4,
+    contrast: 0.04,
+    cycle: 0,
+    pusherCycle: 1,
+    pusherReady: true,
+    frequency: 0.07,
+    phaseEnd: Math.floor(2 * Math.PI * (1 / 0.07)),
+    pushMag: canvas.width * 0.1,
+    drawStep: 25
+  };
+  if (waveOut) canvas.height += 100;
+
+  // add walls
+  function addWall(x, y, width, height) {
+    World.add(engine.world, [
+      Bodies.rectangle(x + width / 2, y + height / 2, width, height, {
+        isStatic: true,
+        friction: 0,
+        frictionAir: 0,
+        frictionStatic: 0,
+        restitution: 1
+      })
+    ]);
+  }
+  const wide = 100;
+  const dropDown = 9;
+  addWall(-wide, -wide + dropDown, canvas.width + 2 * wide, wide); //top wall
+  addWall(-wide, 32 + dropDown, canvas.width + 2 * wide, wide); //bottom wall
+  addWall(canvas.width, -wide, wide, 2 * wide); //left wall
+
+  //add atoms
+  const atom = [];
+  for (let i = 0; i < settings.rows; ++i) {
+    for (let j = 0; j < settings.columns; ++j) {
+      const len = atom.length;
+      atom[len] = Matter.Bodies.polygon(i * settings.radius * 2, j * settings.radius * 2 + 2, 0, settings.radius, {
+        friction: 0,
+        frictionAir: 0,
+        frictionStatic: 0,
+        restitution: 1,
+        inertia: Infinity
+      });
+      const speed = 4;
+      Matter.Body.setVelocity(atom[len], {
+        x: speed * (Math.random() - 0.5),
+        y: speed * (Math.random() - 0.5)
+      });
+      World.add(engine.world, atom[len]);
+    }
+  }
+
+  //pusher plate
+  const pusher = Matter.Bodies.rectangle(-1000, settings.height / 2 - 50, settings.pushMag * 2, settings.height + 200, {
+    isStatic: true,
+    friction: 0,
+    frictionAir: 0,
+    frictionStatic: 0,
+    restitution: 1
+  });
+  World.add(engine.world, pusher);
+
+  const pusherControl = function() {
+    if (!(settings.pusherCycle % settings.phaseEnd)) {
+      settings.pusherReady = false;
+      settings.pusherCycle = 1;
+    }
+    if (settings.pusherReady) settings.pusherCycle++;
+    Matter.Body.setPosition(pusher, { x: settings.pushMag * -Math.cos(settings.pusherCycle * settings.frequency), y: canvas.height / 2 - 50 });
+  };
+
+  //set up array to store wave velocity for graph
+  const amp = [];
+  for (let i = 0, len = Math.floor(canvas.width / settings.drawStep); i < len; ++i) {
+    amp[i] = 0;
+  }
+  const waveOutput = function() {
+    //dampen amp
+    for (let i = 0, len = amp.length; i < len; ++i) {
+      amp[i] *= 0.85;
+    }
+    for (let i = 2, len = atom.length; i < len; ++i) {
+      const max = canvas.width / settings.drawStep - 1;
+      const index = Math.min(Math.max(Math.floor(atom[i].position.x / settings.drawStep), 0), max);
+      amp[index] += 1.15;
+    }
+    //draw graph
+    var grd = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    grd.addColorStop(0.2, "#06a");
+    grd.addColorStop(1, "#fff");
+    ctx.fillStyle = grd; //"#9bc"; //"rgba(150,190,200,1)";
+    const drop = 40;
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height - amp[1] + drop);
+    for (let i = 1, len = amp.length; i < len; i++) {
+      ctx.lineTo(i * settings.drawStep, canvas.height - amp[i] + drop);
+    }
+    ctx.lineTo(canvas.width, canvas.height - amp[amp.length - 1] + drop);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.fill();
+  };
+
+  const escapeCheck = function() {
+    if (!(settings.cycle % 60)) {
+      for (let i = 0, len = atom.length; i < len; ++i) {
+        if (atom[i].position.y > 50 || atom[i].position.y < -50) {
+          Matter.Body.setPosition(atom[i], { x: canvas.width / 2, y: 2 });
+          Matter.Body.setVelocity(atom[i], { x: 0, y: 0 });
+        }
+      }
+    }
+  };
+
+  const draw = function() {
+    if (waveOut) waveOutput();
+    //draw atoms
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = "#fff";
+    if (speedOpacity) {
+      for (let i = 0, len = atom.length; i < len; ++i) {
+        let vertices = atom[i].vertices;
+        ctx.beginPath();
+        ctx.arc(vertices[0].x, vertices[0].y, settings.radius, 0, 2 * Math.PI);
+        // const mag = atom[i].speed + Math.abs(atom[i].angularVelocity * 10); //can't get a value for angular speed not sure why
+        const mag = atom[i].speed; //can't get a value for angular speed not sure why
+        const alpha = Math.max(Math.min(atom[i].speed * atom[i].speed * settings.contrast, 1), 0.05);
+        ctx.fillStyle = "rgba(0,0,0," + alpha + ")";
+        ctx.fill();
+      }
+    } else {
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      for (let i = 0, len = atom.length; i < len; ++i) {
+        let vertices = atom[i].vertices;
+        ctx.beginPath();
+        ctx.arc(vertices[0].x, vertices[0].y, settings.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+      }
+    }
+    //draw pusher
+    let vertices = pusher.vertices;
+    ctx.beginPath();
+    ctx.moveTo(vertices[0].x, vertices[0].y);
+    for (let j = 1; j < vertices.length; j += 1) {
+      ctx.lineTo(vertices[j].x, vertices[j].y);
+    }
+    ctx.lineTo(vertices[0].x, vertices[0].y);
+    ctx.fillStyle = "#f24";
+    ctx.fill();
+  };
+
+  const drawOneAtom = function() {
+    where = atom[Math.floor(atom.length / 3)].position;
+    ctx.beginPath();
+    ctx.arc(where.x, where.y, settings.radius * 1.2, 0, 2 * Math.PI);
+    ctx.fillStyle = "#f24";
+    ctx.strokeStyle = "#fff";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.fill();
+  };
+
+  function cycle() {
+    Engine.update(engine, 16);
+    settings.cycle++;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw();
+    if (drawOne) drawOneAtom();
+    pusherControl();
+    escapeCheck();
+    if (!pause) requestAnimationFrame(cycle);
+  }
+  requestAnimationFrame(cycle);
+}
--- physics/notes/waves/waves/wave.js
+++ physics/notes/waves/waves/wave.js
@@ -3,25 +3,25 @@
 const wave = function() {
   const sineWaveTarget = document.getElementById("sine_wave");
   var origin = {
-    //origin of axes
+    //origin of x,y axes
     x: 0,
     y: 200
   };
   settings = {
-    velocity: 0.8334,
+    velocity: 60,
     amplitude: 60,
     wavelength: 300,
-    frequency: 0.004167, //times 60 for seconds
-    period: 240, //divide by 60 for seconds
+    frequency: 0.2,
+    period: 5,
     phase: 100,
     width: window.innerWidth, //805
-    resolution: 1, // 1 is one to one pixel
     time: 0,
     water: true,
     baseF: 0.02,
     scale: 24
   };
   document.getElementById("sin-wave").setAttribute("width", settings.width);
+
   let pause = true;
   document.getElementById("wave_animation").addEventListener("mouseleave", function() {
     pause = true;
@@ -49,20 +49,20 @@ const wave = function() {
     }
   }
   document.getElementById("sin-wave").addEventListener("click", toggleWater);
-
+  toggleWater();
   (function setup() {
     document.getElementById("amplitude").value = settings.amplitude;
-    document.getElementById("velocity").value = settings.velocity * 60;
+    document.getElementById("velocity").value = settings.velocity;
     document.getElementById("wavelength").value = settings.wavelength;
-    document.getElementById("frequency").innerHTML = "frequency = " + (settings.frequency * 60 * settings.velocity).toFixed(3) + " Hz";
-    document.getElementById("period").innerHTML = "peroid = " + (settings.period / 60).toFixed(3) + " s";
+    document.getElementById("frequency").innerHTML = "frequency = " + settings.frequency.toFixed(3) + " Hz";
+    document.getElementById("period").innerHTML = "period = " + settings.period.toFixed(3) + " s";
     settings.phase = settings.phase % settings.wavelength; //makes the switch smoother
-    settings.frequency = 1 / settings.wavelength;
   })();
+
   document.getElementById("amplitude").addEventListener(
     "input",
     function() {
-      settings.amplitude = document.getElementById("amplitude").value;
+      settings.amplitude = Number(document.getElementById("amplitude").value);
       drawSineWave();
     },
     false
@@ -71,11 +71,11 @@ const wave = function() {
   document.getElementById("velocity").addEventListener(
     "input",
     function() {
-      settings.velocity = document.getElementById("velocity").value / 60;
-      settings.frequency = 1 / settings.wavelength;
+      settings.velocity = Number(document.getElementById("velocity").value);
+      settings.frequency = settings.velocity / settings.wavelength;
       settings.period = 1 / settings.frequency;
-      document.getElementById("frequency").innerHTML = "frequency = " + (settings.frequency * 60 * settings.velocity).toFixed(3) + " Hz";
-      document.getElementById("period").innerHTML = "peroid = " + (settings.period / 60).toFixed(3) + " s";
+      document.getElementById("frequency").innerHTML = "frequency = " + settings.frequency.toFixed(3) + " Hz";
+      document.getElementById("period").innerHTML = "period = " + settings.period.toFixed(3) + " s";
       drawSineWave();
     },
     false
@@ -84,32 +84,21 @@ const wave = function() {
   document.getElementById("wavelength").addEventListener(
     "input",
     function() {
-      settings.wavelength = document.getElementById("wavelength").value;
+      settings.wavelength = Math.max(Number(document.getElementById("wavelength").value), 1);
       settings.phase = settings.phase % settings.wavelength; //makes the switch smoother
-
-      settings.frequency = 1 / settings.wavelength;
+      settings.frequency = settings.velocity / settings.wavelength;
       settings.period = 1 / settings.frequency;
-      document.getElementById("frequency").innerHTML = "frequency = " + (settings.frequency * 60 * settings.velocity).toFixed(3) + " Hz";
-      document.getElementById("period").innerHTML = "peroid = " + (settings.period / 60).toFixed(3) + " s";
-
+      document.getElementById("frequency").innerHTML = "frequency = " + settings.frequency.toFixed(3) + " Hz";
+      document.getElementById("period").innerHTML = "period = " + settings.period.toFixed(3) + " s";
       drawSineWave();
     },
     false
   );
-  //adds sine wave path to svg
-  // function drawSineWave() {
-  //   let d = "M-1 " + (-Math.sin(settings.frequency * 2 * Math.PI * (-1 + settings.phase)) * settings.amplitude + origin.y);
-  //   for (let i = 0; i < settings.width; i += settings.resolution) {
-  //     d += " L" + i + " " + (-Math.sin(settings.frequency * 2 * Math.PI * (i - 1 + settings.phase)) * settings.amplitude + origin.y);
-  //   }
-  //   // d += "L" + settings.width + "-200 L0 -200";
-  //   d += "V500 L-100 500";
-  //   sineWaveTarget.setAttribute("d", d);
-  // }
+
   function drawSineWave() {
-    let d = "M-1 " + (-Math.sin(settings.frequency * 2 * Math.PI * (-1 + settings.phase)) * settings.amplitude + origin.y);
-    for (let x = 0; x < settings.width; x += settings.resolution) {
-      d += " L" + x + " " + (-Math.sin(settings.frequency * 2 * Math.PI * (x - 1 + settings.phase)) * settings.amplitude + origin.y);
+    let d = "M-1 " + (-Math.sin(2 * Math.PI / settings.wavelength * (-1 + settings.phase)) * settings.amplitude + origin.y);
+    for (let x = 0; x < settings.width; ++x) {
+      d += " L" + x + " " + (-Math.sin(2 * Math.PI / settings.wavelength * (x - 1 + settings.phase)) * settings.amplitude + origin.y);
     }
     d += "V500 L-100 500";
     sineWaveTarget.setAttribute("d", d);
@@ -117,15 +106,9 @@ const wave = function() {
   drawSineWave();
   function render() {
     //repeating animation function
-    settings.phase -= settings.velocity;
-    settings.time++;
-    document.getElementById("time").innerHTML = (settings.time / 60).toFixed(1) + "s";
-
-    // settings.baseF += 0.000015 * Math.sin(settings.time * 0.005);
-    // document.getElementById("fe-turb").setAttribute("baseFrequency", settings.baseF);
-    // settings.scale += 0.04 * Math.sin(settings.time * 0.005 + Math.PI);
-    // document.getElementById("fe-disp").setAttribute("scale", settings.scale);
-
+    settings.phase -= settings.velocity / 60;
+    settings.time += 1 / 60;
+    document.getElementById("time").innerHTML = settings.time.toFixed(1) + "s";
     drawSineWave();
     if (!pause) window.requestAnimationFrame(render);
   }
