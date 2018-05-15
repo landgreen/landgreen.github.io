57185b0c912081d9ed27483bc4624e152f8e639c
landgreen <rosslandgreen@gmail.com>
2018-05-15 08:54:48 -0700

    Add files via upload

--- physics/notes/relativity/special/solver.js
+++ physics/notes/relativity/special/solver.js
@@ -4,12 +4,11 @@ function checkVisible(elm) {
   return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
-function drawRocket(ctx, x, y, time = 0, scale = 1) {
+function drawRocket(ctx, x, y, time = 0, scale = 1, period = 5) {
   //move canvas to the ship and compress in the horizontal direction
   ctx.save();
   ctx.translate(x, y);
   ctx.scale(scale, 1);
-
   //rocket fins
   ctx.fillStyle = "#D90429";
   ctx.beginPath();
@@ -48,14 +47,13 @@ function drawRocket(ctx, x, y, time = 0, scale = 1) {
   //clock outline
   ctx.fillStyle = "#D3DBDE";
   ctx.beginPath();
-  ctx.arc(80, 50, 45, 0, 2 * Math.PI * 60);
+  ctx.arc(80, 50, 45, 0, 2 * Math.PI);
   ctx.fill();
   //clock
-
   ctx.fillStyle = "#2B2D42";
   ctx.beginPath();
   ctx.moveTo(80, 50);
-  ctx.arc(80, 50, 45, 0, Math.round(time % (2 * Math.PI * 60)) / 60);
+  ctx.arc(80, 50, 45, 0, Math.floor(time % (period * 20 * Math.PI)) / 10 / period);
   ctx.fill();
   //undo canvas transformations
   ctx.restore();
@@ -73,16 +71,16 @@ let solver = function() {
     const v = document.getElementById("velocity").value;
     const time = Number(document.getElementById("time").value);
     const dist = Number(document.getElementById("length").value);
-    const mass = Number(document.getElementById("mass").value);
+    // const mass = Number(document.getElementById("mass").value);
     rockets.gamma = 1 / Math.sqrt(1 - v * v);
     const vTime = time * rockets.gamma;
     const vDist = dist / rockets.gamma;
-    const vMass = mass * rockets.gamma;
+    // const vMass = mass * rockets.gamma;
     document.getElementById("velocity2").value = v * 3;
     document.getElementById("gamma").value = Math.round(rockets.gamma * 1000000000000) / 1000000000000;
     document.getElementById("v_time").value = vTime.toFixed(4);
     document.getElementById("v_length").value = vDist.toFixed(4);
-    document.getElementById("v_mass").value = vMass.toFixed(4);
+    // document.getElementById("v_mass").value = vMass.toFixed(4);
     // document.getElementById("length1").style.width = dist * 10 + "px";
     // document.getElementById("length2").style.width = vDist * 10 + "px";
   }
@@ -92,15 +90,15 @@ let solver = function() {
     rockets.gamma = 1 / Math.sqrt(1 - v * v);
     const vTime = Number(document.getElementById("v_time").value);
     const vDist = Number(document.getElementById("v_length").value);
-    const vMass = Number(document.getElementById("v_mass").value);
+    // const vMass = Number(document.getElementById("v_mass").value);
     const time = vTime / rockets.gamma;
     const dist = vDist * rockets.gamma;
-    const mass = vMass / rockets.gamma;
+    // const mass = vMass / rockets.gamma;
     document.getElementById("velocity2").value = v * 3;
     document.getElementById("gamma").value = Math.round(rockets.gamma * 1000000000000) / 1000000000000;
     document.getElementById("time").value = time.toFixed(4);
     document.getElementById("length").value = dist.toFixed(4);
-    document.getElementById("mass").value = mass.toFixed(4);
+    // document.getElementById("mass").value = mass.toFixed(4);
     // document.getElementById("length1").style.width = dist * 10 + "px";
     // document.getElementById("length2").style.width = vDist * 10 + "px";
   }
@@ -121,10 +119,10 @@ let solver = function() {
   document.getElementById("gamma").addEventListener("input", convertGamma);
   document.getElementById("time").addEventListener("input", update);
   document.getElementById("length").addEventListener("input", update);
-  document.getElementById("mass").addEventListener("input", update);
+  // document.getElementById("mass").addEventListener("input", update);
   document.getElementById("v_time").addEventListener("input", updateMoving);
   document.getElementById("v_length").addEventListener("input", updateMoving);
-  document.getElementById("v_mass").addEventListener("input", updateMoving);
+  // document.getElementById("v_mass").addEventListener("input", updateMoving);
 
   //draw the clocks
   var canvas = document.getElementById("rockets");
@@ -132,11 +130,14 @@ let solver = function() {
   //___________________animation loop ___________________
   function cycle() {
     if (checkVisible(canvas) && rockets.isOn) {
-      rockets.t1 += 1 / rockets.gamma;
-      rockets.t2++;
+      if (document.getElementById("time").value != 0) {
+        const timeScale = document.getElementById("time").value / 10;
+        rockets.t1 += 1 / rockets.gamma / timeScale;
+        rockets.t2 += 1 / timeScale;
+      }
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       drawRocket(ctx, 20, 20, rockets.t1, 1 / rockets.gamma);
-      drawRocket(ctx, 320, 20, rockets.t2);
+      drawRocket(ctx, 320, 20, rockets.t2, 1);
     }
     requestAnimationFrame(cycle);
   }
@@ -163,7 +164,7 @@ const relative = function() {
   const settings = {
     wallWidth: 200,
     time: 0,
-    moveScale: 1.4
+    moveScale: 2
   };
 
   const rocket1 = {
