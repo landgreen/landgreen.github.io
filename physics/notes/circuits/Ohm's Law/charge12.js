ea69a52887edb5781c4d5353378b9df2cf3fa136
lilgreenland <rosslandgreen@gmail.com>
2018-06-28 15:46:03 -0700

    units

--- physics/notes/circuits/Ohm's Law/charge12.js
+++ physics/notes/circuits/Ohm's Law/charge12.js
@@ -1,7 +1,7 @@
 (function() {
   var canvas = document.getElementById("charge12");
   var ctx = canvas.getContext("2d");
-  ctx.font = "30px Arial";
+  ctx.font = "22px Arial";
   ctx.fillStyle = "#aaa";
   ctx.textAlign = "center";
   ctx.fillText("click to start simulation", canvas.width / 2, canvas.height / 2);
