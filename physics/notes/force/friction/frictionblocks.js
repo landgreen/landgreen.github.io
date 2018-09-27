5f9fccd736bf27482a7b8dbba7d67215fcbc07c9
lilgreenland <rosslandgreen@gmail.com>
2018-09-27 15:41:42 -0700

    friction changes

--- physics/notes/force/friction/frictionblocks.js
+++ physics/notes/force/friction/frictionblocks.js
@@ -12,7 +12,8 @@ let blocks = () => {
             y: 0
         },
         mass: 5,
-        mu: 0.03,
+        muStatic: 0.61,
+        muKinetic: 0.47,
         moving: false,
     }
     let rightBlock = {
@@ -26,26 +27,54 @@ let blocks = () => {
             x: 0,
             y: 0
         },
-        mass: 0.01
+        mass: 0
     }
-
 
     document.getElementById("mass").addEventListener("input", () => {
             rightBlock.mass = Number(document.getElementById("mass").value)
             document.getElementById("mass-slider").value = rightBlock.mass
-            if (!leftBlock.moving && rightBlock.mass > maxStaticFriction) requestAnimationFrame(cycle);
-
+            // if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic && rightBlock.mass > leftBlock.muKinetic) requestAnimationFrame(cycle);
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
         },
         false
     );
     document.getElementById("mass-slider").addEventListener("input", () => {
             rightBlock.mass = Number(document.getElementById("mass-slider").value)
             document.getElementById("mass").value = rightBlock.mass
-            if (!leftBlock.moving && rightBlock.mass > maxStaticFriction) requestAnimationFrame(cycle);
-
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
         },
         false
     );
+    document.getElementById("mu").addEventListener("input", () => {
+            leftBlock.muStatic = Number(document.getElementById("mu").value)
+            document.getElementById("mu-slider").value = leftBlock.muStatic
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
+        },
+        false
+    );
+    document.getElementById("mu-slider").addEventListener("input", () => {
+            leftBlock.muStatic = Number(document.getElementById("mu-slider").value)
+            document.getElementById("mu").value = leftBlock.muStatic
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
+        },
+        false
+    );
+
+    document.getElementById("mu-k").addEventListener("input", () => {
+            leftBlock.muKinetic = Number(document.getElementById("mu-k").value)
+            document.getElementById("mu-k-slider").value = leftBlock.muKinetic
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
+        },
+        false
+    );
+    document.getElementById("mu-k-slider").addEventListener("input", () => {
+            leftBlock.muKinetic = Number(document.getElementById("mu-k-slider").value)
+            document.getElementById("mu-k").value = leftBlock.muKinetic
+            if (!leftBlock.moving && rightBlock.mass > leftBlock.muStatic * leftBlock.mass && rightBlock.mass > leftBlock.muKinetic * leftBlock.mass) requestAnimationFrame(cycle);
+        },
+        false
+    );
+
     document.getElementById("reset-blocks").addEventListener("click", () => {
             leftBlock.position.x = 150
             leftBlock.velocity.x = 0
@@ -53,15 +82,22 @@ let blocks = () => {
             rightBlock.position.y = 70
             rightBlock.velocity.y = 0
             rightBlock.mass = 0.01
+            leftBlock.muKinetic = 0.47
+            leftBlock.muStatic = 0.61
             document.getElementById("mass").value = rightBlock.mass
             document.getElementById("mass-slider").value = rightBlock.mass
+            document.getElementById("mu").value = leftBlock.muStatic
+            document.getElementById("mu-slider").value = leftBlock.muStatic
+            document.getElementById("mu-k").value = leftBlock.muKinetic
+            document.getElementById("mu-k-slider").value = leftBlock.muKinetic
             draw()
         },
         false
     );
 
     function physics() {
-        const a = 9.8 * (0.1 - rightBlock.mass) / (rightBlock.mass + 5)
+        const a = 9.8 * (leftBlock.mass * leftBlock.muKinetic - rightBlock.mass) / (rightBlock.mass + leftBlock.mass)
+        // const a = 9.8 * (5 * leftBlock.muKinetic - rightBlock.mass) / (5 - rightBlock.mass)
         leftBlock.velocity.x = rightBlock.velocity.y -= a / 60 / 4; // /60 for the frame rate  /4 just for looks
 
         if (leftBlock.velocity.x > 0) {
