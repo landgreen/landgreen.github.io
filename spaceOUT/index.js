6efd8806ec650585c377d4038f95b7bfda454898
lilgreenland <rosslandgreen@gmail.com>
2019-02-26 15:29:45 -0800

    tron ideas

--- spaceOUT/index.js
+++ spaceOUT/index.js
@@ -16,48 +16,47 @@ window.addEventListener("resize", resizeCanvas);
 
 
 //___________________get keyboard input___________________
-// const keys = [];
-// document.addEventListener("keydown", event => {
-//     keys[event.keyCode] = true;
-//     // console.log(event.keyCode);
-// });
-// document.addEventListener("keyup", event => {
-//     keys[event.keyCode] = false;
-// });
+const keys = [];
+document.addEventListener("keydown", event => {
+    keys[event.keyCode] = true;
+    console.log(event.keyCode);
+});
+document.addEventListener("keyup", event => {
+    keys[event.keyCode] = false;
+});
 
+const p1 = {
+    color: [255, 150, 60], // [R,G,B]
+    position: {
+        x: canvas.width * 0.8,
+        y: canvas.height / 2
+    },
+    velocity: {
+        x: -1,
+        y: 0
+    }
+}
 
-// const p1 = {
-//     color: [255, 255, 255],
-//     position: {
-//         x: 100,
-//         y: 100
-//     },
-//     velocity: {
-//         x: 0,
-//         y: 0
-//     }
-// }
-
-// const p2 = {
-//     color: [0, 0, 0],
-//     position: {
-//         x: 300,
-//         y: 100
-//     },
-//     velocity: {
-//         x: 0,
-//         y: 0
-//     }
-// }
+const p2 = {
+    color: [0, 255, 255], // [R,G,B]
+    position: {
+        x: canvas.width * 0.2,
+        y: canvas.height / 2
+    },
+    velocity: {
+        x: 1,
+        y: 0
+    }
+}
 
 
 var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 var data = imageData.data;
 
-
 function setColor() {
     for (var i = 0; i < data.length; i += 4) {
-        const color = Math.random() > 0.5 ? 255 : 0;
+        // const color = Math.random() > 0.5 ? 255 : 0;
+        const color = 0
         data[i] = color; // red
         data[i + 1] = color; // green
         data[i + 2] = color; // blue
@@ -65,22 +64,117 @@ function setColor() {
     }
 }
 
+function turn() {
+    function rotate(v, angle) {
+        const sin = Math.sin(angle)
+        const cos = Math.cos(angle)
+        return {
+            x: v.x * cos - v.y * sin,
+            y: v.x * sin + v.y * cos
+        }
+    }
+    const turnRate = 0.05
+
+    if (keys[37]) {
+        const turn = rotate(p1.velocity, -turnRate)
+        p1.velocity.x = turn.x
+        p1.velocity.y = turn.y
+    } else if (keys[39]) {
+        const turn = rotate(p1.velocity, turnRate)
+        p1.velocity.x = turn.x
+        p1.velocity.y = turn.y
+    }
+
+    if (keys[65]) {
+        const turn = rotate(p2.velocity, -turnRate)
+        p2.velocity.x = turn.x
+        p2.velocity.y = turn.y
+    } else if (keys[68]) {
+        const turn = rotate(p2.velocity, turnRate)
+        p2.velocity.x = turn.x
+        p2.velocity.y = turn.y
+    }
+}
+
+function move() {
+    p1.position.x += p1.velocity.x
+    p1.position.y += p1.velocity.y
+
+    p2.position.x += p2.velocity.x
+    p2.position.y += p2.velocity.y
+}
+
+function edgeDie() {
+    if (p1.position.x < 0 || p1.position.x > canvas.width || p1.position.y < 0 || p1.position.y > canvas.height) {
+        console.log("p1 crash wall")
+    }
+    if (p2.position.x < 0 || p2.position.x > canvas.width || p2.position.y < 0 || p2.position.y > canvas.height) {
+        console.log("p2 crash wall")
+    }
+}
+// function edgeWrap() {
+//     if (p1.position.x < 0) {
+//         p1.position.x = canvas.width
+//     } else if (p1.position.x > canvas.width) {
+//         p1.position.x = 0
+//     }
+//     if (p1.position.y < 0) {
+//         p1.position.y = canvas.height
+//     } else if (p1.position.y > canvas.height) {
+//         p1.position.y = 0
+//     }
+
+//     if (p2.position.x < 0) {
+//         p2.position.x = canvas.width
+//     } else if (p2.position.x > canvas.width) {
+//         p2.position.x = 0
+//     }
+//     if (p2.position.y < 0) {
+//         p2.position.y = canvas.height
+//     } else if (p2.position.y > canvas.height) {
+//         p2.position.y = 0
+//     }
+// }
+
+function collision() { //get color of tile player in about to hit
+    let index
+
+    index = 4 * (Math.floor(p1.position.x) + Math.floor(p1.position.y) * canvas.width)
+    if (data[index] === p2.color[0] && data[index + 1] === p2.color[1] && data[index + 2] === p2.color[2]) {
+        console.log("p1 crash")
+    }
+
+    index = 4 * (Math.floor(p2.position.x) + Math.floor(p2.position.y) * canvas.width)
+    if (data[index] === p1.color[0] && data[index + 1] === p1.color[1] && data[index + 2] === p1.color[2]) {
+        console.log("p2 crash")
+    }
+}
+
 function drawPlayers() {
     //add player 1 position
     const imgIndex1 = 4 * (Math.floor(p1.position.x) + Math.floor(p1.position.y) * canvas.width);
-    data[imgIndex1] = 0; // red
-    data[imgIndex1 + 1] = 0; // green
-    data[imgIndex1 + 2] = 0; // blue
+    data[imgIndex1] = p1.color[0]; // red
+    data[imgIndex1 + 1] = p1.color[1]; // green
+    data[imgIndex1 + 2] = p1.color[2]; // blue
 
     //add player 2 position
     const imgIndex2 = 4 * (Math.floor(p2.position.x) + Math.floor(p2.position.y) * canvas.width);
-    data[imgIndex2] = 255; // red
-    data[imgIndex2 + 1] = 255; // green
-    data[imgIndex2 + 2] = 255; // blue
+    data[imgIndex2] = p2.color[0]; // red
+    data[imgIndex2 + 1] = p2.color[1]; // green
+    data[imgIndex2 + 2] = p2.color[2]; // blue
 }
 
 function isPixelOn(x, y) {
     return data[4 * (x + y * canvas.width)]
+}
+
+function fadeAway() {
+    for (var i = 0; i < data.length; i += 4) {
+        const index = i
+        if (data[index] > 0) data[index]--
+        if (data[index + 1] > 0) data[index + 1]--
+        if (data[index + 2] > 0) data[index + 2]--
+    }
 }
 
 function nightDay() { // B3678/S34678
@@ -126,8 +220,13 @@ setColor();
 
 
 function cycle() {
-    // drawPlayers();
-    nightDay();
+    turn()
+    move()
+    edgeDie()
+    collision();
+    drawPlayers();
+    // nightDay();
+    // if (keys[32]) fadeAway()
     ctx.putImageData(imageData, 0, 0);
     requestAnimationFrame(cycle);
 }
