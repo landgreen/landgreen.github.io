d0537aa1a1b57920a411a1038319ae2c7d2e5e1a
landgreen <rosslandgreen@gmail.com>
2019-04-28 17:50:05 -0700

    added auto search

--- physics/notes/waves/waveproperties/standingwave.js
+++ physics/notes/waves/waveproperties/standingwave.js
@@ -15,10 +15,13 @@ const wave = function () {
         x: 0,
         y: 200
     };
+    let search = false; //how much the wavelength changes each cycle
+    let target = 600;
+    let searchStep = 1;
     const velocity = 400;
-    let reflections = 32
-    const totalPaths = 18 //how many paths are drawn in the SVG, the rest are just calculated for the superposition path
-    let dampening = -1 //lower values simulate energy loss, but don't make the standing waves as distinct
+    let reflections = 64;
+    const totalPaths = 18; //how many paths are drawn in the SVG, the rest are just calculated for the superposition path
+    let dampening = -1; //lower values simulate energy loss, but don't make the standing waves as distinct
     let wavelength = 6 * 100;
     document.getElementById("wavelength").value = wavelength / 100;
 
@@ -31,10 +34,36 @@ const wave = function () {
     document.getElementById("standing-wave-border").setAttribute("d", "M0 0 v400 M" + width + " 0 v400");
     //update answer to question based on width
     let out = "λ = "
-    for (let i = 1; i < 23; i++) {
+    let standingWavelengths = []
+    for (let i = 1; i < 50; i++) {
+        standingWavelengths.push(Math.round(20 * width / i) / 10)
         out += Math.round(2 * width / 100 / i * 1000) / 1000 + ", &nbsp;"
+        if (standingWavelengths[standingWavelengths.length - 1] < 80) break
     }
     document.getElementById("list-standing").innerHTML = out + "..."
+
+    document.getElementById("wave-search").addEventListener("click", () => {
+        if (search) {
+            search = false;
+        } else {
+            search = true;
+
+            // identify the next target
+            const totalSteps = 120
+            for (let i = standingWavelengths.length; i > -1; i--) {
+                if (standingWavelengths[i] > wavelength) {
+                    target = standingWavelengths[i]
+                    searchStep = (target - wavelength) / totalSteps
+                    return;
+                }
+            }
+            //in case it doesn't find anything
+            wavelength = standingWavelengths[standingWavelengths.length - 1]
+            target = standingWavelengths[standingWavelengths.length - 2];
+            searchStep = (target - wavelength) / totalSteps;
+        }
+    }, false);
+
 
     document.getElementById("wavelength").addEventListener("input", () => {
         wavelength = Math.max(Number(document.getElementById("wavelength").value) * 100, 1);
@@ -85,31 +114,32 @@ const wave = function () {
 
         for (let j = 0; j < reflections; j += 2) {
             //reflected wave (starts on left)
+            const draw = (j < totalPaths) ? true : false;
             amplitude *= dampening
             offset += width
 
-            wave = Math.sin(frequency * (offset - phase)) * amplitude
+            wave = amplitude * Math.sin(frequency * (offset - phase))
             sum[0] += wave //adding wave contribution to superposition wave
-            if (j < totalPaths) d = "M-1 " + (wave + origin.y);;
+            if (draw) d = "M-1 " + (wave + origin.y);;
             for (let x = 0; x < width; x++) {
                 wave = Math.sin(frequency * (x + offset - phase)) * amplitude
                 sum[x] += wave //adding wave contribution to superposition wave
-                if (j < totalPaths) d += " L" + x + " " + (wave + origin.y);
+                if (draw) d += " L" + x + " " + (wave + origin.y);
             }
-            if (j < totalPaths) document.getElementById("wave-form-path-" + j).setAttribute("d", d);
+            if (draw) document.getElementById("wave-form-path-" + j).setAttribute("d", d);
 
             //reflected wave (starts on right)
             amplitude *= dampening
             offset += width
 
-            wave = Math.sin(frequency * (offset - phase)) * amplitude
-            if (j < totalPaths) d = "M" + width + " " + (wave + origin.y);;
+            wave = amplitude * Math.sin(frequency * (offset - phase))
+            if (draw) d = "M" + width + " " + (wave + origin.y);;
             for (let i = 0, x = width; i < width; i++, x++) {
                 wave = Math.sin(frequency * (x + offset - width - phase)) * amplitude
                 sum[width - i] += wave; //adding wave contribution to superposition wave
-                if (j < totalPaths) d += " L" + (width - i) + " " + (wave + origin.y);;
+                if (draw) d += " L" + (width - i) + " " + (wave + origin.y);;
             }
-            if (j < totalPaths) document.getElementById("wave-form-path-" + (j + 1)).setAttribute("d", d);
+            if (draw) document.getElementById("wave-form-path-" + (j + 1)).setAttribute("d", d);
         }
 
         //draw interference patter superposition
@@ -122,6 +152,20 @@ const wave = function () {
         document.getElementById("superposition-wave-form-path").setAttribute("d", d);
     }
 
+    function waveSearch() {
+        if (search) {
+            wavelength += searchStep
+            if (wavelength > target) {
+                wavelength = target
+                search = false;
+            }
+            document.getElementById("wavelength-slider").value = wavelength / 100
+            document.getElementById("wavelength").value = wavelength / 100
+            phase = phase % wavelength; //makes the switch smoother
+        }
+    }
+
+
     let pause = true;
     const elZone = document.getElementById("standing-wave-zone");
     elZone.addEventListener("mouseleave", () => {
@@ -133,10 +177,13 @@ const wave = function () {
             requestAnimationFrame(cycle);
         }
     });
+    time = 0
 
     function cycle() {
+        time++
         if (!pause) requestAnimationFrame(cycle);
         draw();
+        waveSearch();
     }
     requestAnimationFrame(cycle); //starts game loop
 
